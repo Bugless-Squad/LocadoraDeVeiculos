d0382859213940d5a850378e0f8919d0a6808c10
Jesiane Straubel <jesi1951@hotmail.com>
2023-08-02 13:28:39 -0300

    Iniciado a tela.

--- LocadoraDeVeiculos.WinApp/ModuloCliente/TelaClienteForm.Designer.cs
+++ LocadoraDeVeiculos.WinApp/ModuloCliente/TelaClienteForm.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnCancelar = new Button();
+            btnGravar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancelar.DialogResult = DialogResult.Cancel;
+            btnCancelar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCancelar.ImageAlign = ContentAlignment.BottomRight;
+            btnCancelar.Location = new Point(645, 227);
+            btnCancelar.Margin = new Padding(4);
+            btnCancelar.MinimumSize = new Size(94, 41);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(94, 41);
+            btnCancelar.TabIndex = 75;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.TextImageRelation = TextImageRelation.TextAboveImage;
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            btnGravar.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnGravar.DialogResult = DialogResult.OK;
+            btnGravar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnGravar.ImageAlign = ContentAlignment.BottomRight;
+            btnGravar.Location = new Point(543, 227);
+            btnGravar.Margin = new Padding(4);
+            btnGravar.MinimumSize = new Size(94, 41);
+            btnGravar.Name = "btnGravar";
+            btnGravar.Size = new Size(94, 41);
+            btnGravar.TabIndex = 74;
+            btnGravar.Text = "Gravar";
+            btnGravar.TextImageRelation = TextImageRelation.TextAboveImage;
+            btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 76;
+            label1.Text = "Telefone:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 77;
+            label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(53, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 78;
+            label3.Text = "Nome:";
+            // 
+            // TelaClienteForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 322);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnGravar);
+            Name = "TelaClienteForm";
+            ShowIcon = false;
+            Text = "Cadastro de Cliente";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCancelar;
+        private Button btnGravar;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
