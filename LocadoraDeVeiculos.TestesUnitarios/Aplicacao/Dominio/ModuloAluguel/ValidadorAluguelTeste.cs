1aac00d587916b1c5d8cc33a16103d5073870348
Dudu403 <eduardomoreira300@outlook.com>
2023-08-10 09:40:34 -0300

    mais testes2

---
+++ LocadoraDeVeiculos.TestesUnitarios/Aplicacao/Dominio/ModuloAluguel/ValidadorAluguelTeste.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using FluentAssertions;
+using FluentValidation.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using LocadoraDeVeiculos.Dominio.ModuloAluguel;
+using LocadoraDeVeiculos.Dominio.ModuloAutomovel;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Dominio.ModuloCupomEParceiro;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoAutomovel;
+using LocadoraDeVeiculos.Dominio.ModuloTaxaEServico;
+using FluentValidation.TestHelper;
+
+namespace LocadoraDeVeiculos.TestesUnitarios.Dominio.ModuloAluguel
+{
+    [TestClass]
+    public class AluguelTest
+    {
+        private Aluguel aluguel;
+        private ValidadorAluguel validadorAluguel;
+
+        public AluguelTest()
+        {
+            aluguel = new Aluguel
+            {
+
+            };
+
+            validadorAluguel = new ValidadorAluguel();
+        }
+
+        [TestMethod]
+        public void Deve_validar_aluguel_com_propriedades_corretas()
+        {
+            ValidationResult result = validadorAluguel.Validate(aluguel);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Deve_invalidar_aluguel_com_valor_total_previsto_negativo()
+        {
+            aluguel.valorTotalPrevisto = -200;
+
+            var result = validadorAluguel.TestValidate(aluguel);
+
+            result.ShouldHaveValidationErrorFor(a => a.valorTotalPrevisto);
+        }
+
+        [TestMethod]
+        public void Deve_invalidar_aluguel_com_nivel_tanque_litros_negativo()
+        {
+            aluguel.nivelTanqueLitros = -20;
+
+            var result = validadorAluguel.TestValidate(aluguel);
+
+            result.ShouldHaveValidationErrorFor(a => a.nivelTanqueLitros);
+        }
+
+        public void Deve_invalidar_aluguel_com_condutor_nulo()
+        {
+            aluguel.condutor = null;
+
+            var result = validadorAluguel.TestValidate(aluguel);
+
+            result.ShouldHaveValidationErrorFor(a => a.condutor);
+        }
+
+
+    }
+}
---
+++ LocadoraDeVeiculos.TestesUnitarios/Dominio/ModuloAluguel/ServicoAluguelTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentResults;
+using LocadoraDeVeiculos.Aplicacao.ModuloAluguel;
+using LocadoraDeVeiculos.Dominio.ModuloAluguel;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.TestesUnitarios.Dominio.ModuloAluguel
+{
+    [TestClass]
+    public class ServicoAluguelTest
+    {
+        Mock<IRepositorioAluguel> repositorioAluguelMoq;
+        Mock<IValidadorAluguel> validadorMoq;
+        Mock<IContextoPersistencia> contextoMoq;
+
+        private ServicoAluguel servicoAluguel;
+
+        Aluguel aluguel;
+
+        [TestInitialize]
+        public void InicializarTeste()
+        {
+            repositorioAluguelMoq = new Mock<IRepositorioAluguel>();
+            validadorMoq = new Mock<IValidadorAluguel>();
+            contextoMoq = new Mock<IContextoPersistencia>();
+            servicoAluguel = new ServicoAluguel(repositorioAluguelMoq.Object, validadorMoq.Object, contextoMoq.Object);
+            aluguel = new Aluguel();
+        }
+
+    }
+}
